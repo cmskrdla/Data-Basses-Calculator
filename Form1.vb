200af602b6a73fae7d649a8130b2c26495bd2d43
CSkrdla <cmskrdla@gmail.com>
2024-08-01 09:45:40 -0500

    Added the same stuff? idk

--- Form1.vb
+++ Form1.vb
@@ -18,6 +18,42 @@
     Private Sub btnPi_Click(sender As Object, e As EventArgs) Handles btnPi.Click
 
     End Sub
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        TextBox1.Text = "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        TextBox1.Text = "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        TextBox1.Text = "4"
+
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        TextBox1.Text = "5"
+
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        TextBox1.Text = "6"
+
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        TextBox1.Text = "7"
+
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        TextBox1.Text = "8"
+
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        TextBox1.Text = "9"
+
 
     ''button1 will be the 0 button
     ''button2 will be the 1 btn
