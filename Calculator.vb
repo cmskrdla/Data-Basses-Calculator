81fc1936af60c6e1aac37cb22f204bcc7fac78dd
bskrdla <bernadette.t.skrdla@gmail.com>
2024-07-31 23:48:24 -0500

    Added Calculator class

---
+++ Calculator.vb
@@ -0,0 +1,31 @@
+﻿Public Class Calculator
+    Implements ICalculator
+
+    Public Sub New()
+
+    End Sub
+
+    Public Function Add(number1 As Double, number2 As Double) As Double Implements ICalculator.Add
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function Divide(number1 As Double, number2 As Double) As Double Implements ICalculator.Divide
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function Multiply(number1 As Double, number2 As Double) As Double Implements ICalculator.Multiply
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function Squared(number1 As Double) As Double Implements ICalculator.Squared
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function SquareRoot(number1 As Double) As Double Implements ICalculator.SquareRoot
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function Subtract(number1 As Double, number2 As Double) As Double Implements ICalculator.Subtract
+        Throw New NotImplementedException()
+    End Function
+End Class
