c941fa5344032c0e4e8a75138e7baa038e030969
CSkrdla <cmskrdla@gmail.com>
2024-08-01 09:44:34 -0500

    Added some stuff for the button clicks

--- Form1.vb
+++ Form1.vb
@@ -24,30 +24,36 @@
     End Sub
 
     Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
-
+        TextBox1.Text = "3"
     End Sub
 
     Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        TextBox1.Text = "4"
 
     End Sub
 
     Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        TextBox1.Text = "5"
 
     End Sub
 
     Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        TextBox1.Text = "6"
 
     End Sub
 
     Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        TextBox1.Text = "7"
 
     End Sub
 
     Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        TextBox1.Text = "8"
 
     End Sub
 
     Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        TextBox1.Text = "9"
 
     End Sub
 
