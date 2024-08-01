3cfe3044a24a7b0149e7ef01e461f2d7f2783508
bskrdla <bernadette.t.skrdla@gmail.com>
2024-07-31 23:44:18 -0500

    Added interfaces from Mr. Skrdla #interfaces

---
+++ iCalculator.vb
@@ -0,0 +1,8 @@
+﻿Public Interface ICalculator
+    Function Add(number1 As Double, number2 As Double) As Double
+    Function Divide(number1 As Double, number2 As Double) As Double
+    Function Multiply(number1 As Double, number2 As Double) As Double
+    Function Squared(number1 As Double) As Double
+    Function SquareRoot(number1 As Double) As Double
+    Function Subtract(number1 As Double, number2 As Double) As Double
+End Interface
